2edf7bd0e98e829971510beee3227e4dd3ea30c5
imscs21 <imscs21@naver.com>
2017-03-09 13:15:57 +0900

    modify data

--- program.py
+++ program.py
@@ -1,4 +1,7 @@
+def printMsg(n):
+    if(n==1):
+        print("my name is Hwang se hyeon")
+    elif(n==2):
+        print("let me introduce myself")
 n = int(input())
-if(n==1):
-    print("my name is Hwang se hyeon")
-
+printMsg(n)
